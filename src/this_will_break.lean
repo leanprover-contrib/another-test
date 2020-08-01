a01a0d293253afce6db5548ddf6abbe920430fc7
Rob Lewis <Rob.y.lewis@gmail.com>
2020-08-01 14:04:28 +0200

    Update this_will_break.lean

--- src/this_will_break.lean
+++ src/this_will_break.lean
@@ -1,3 +1,3 @@
 import data.real.ennreal 
 
-def ennreal.of_real_lt_top := 2  -- will break with https://github.com/leanprover-community/mathlib/pull/3644/files
+-- def ennreal.of_real_lt_top := 2  -- will break with https://github.com/leanprover-community/mathlib/pull/3644/files
